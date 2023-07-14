08b55bd1b6d427d1969a563afe47b2ac493aa801
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-15 02:00:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import flet as ft
 import os
 from supabase import create_client, Client
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
 
 def main(page: ft.Page):
+    
+    url: str = os.environ.get("SUPABASE_URL")
+    key: str = os.environ.get("SUPABASE_KEY")
+    supabase: Client = create_client(url, key)
+    
     aa = ft.Text('안녕하세요')
     page.add(aa)
     pass
