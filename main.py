1e08ea17088a9a44bdb9fa5e1eeda3e8a889218a
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-15 01:11:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from supabase import create_client, Client
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
-# supabase: Client = create_client(url, key)
+supabase: Client = create_client(url, key)
 
 def main(page: ft.Page):
     aa = ft.Text('안녕하세요')
